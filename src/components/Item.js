0576b53827afd85f7991f2b02443722ad4168672
“Marc <marc_piantone@live.fr>
2020-07-23 14:11:15 -0400

    Changed Item div for Item button

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const ItemDiv = styled.div`
+const ItemButton = styled.button`
   line-height: 2em;
 `;
 
@@ -13,12 +13,12 @@ const Text = styled.p``;
 
 const Item = ({ item, purchasedItems, handleClick }) => {
   return (
-    <ItemDiv onClick={() => handleClick(item)}>
+    <ItemButton onClick={() => handleClick(item)}>
       <Title>{item.name}</Title>
       <Text>
         Cost {item.cost} cookie(s). Produces {item.value} cookies/second.
       </Text>
-    </ItemDiv>
+    </ItemButton>
   );
 };
 
